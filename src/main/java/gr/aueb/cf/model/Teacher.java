5e050d47321cb759679856e6a05baf5055ebd7d3
Th. Androutsos <a8anassis@gmail.com>
2025-04-24 21:04:55 +0300

    feat: updates

--- src/main/java/gr/aueb/cf/model/Teacher.java
+++ src/main/java/gr/aueb/cf/model/Teacher.java
@@ -37,7 +37,7 @@ public class Teacher {
     @ManyToMany(mappedBy = "teachers", fetch = FetchType.LAZY)
     private Set<Course> courses = new HashSet<>();
 
-    @OneToOne(fetch = FetchType.LAZY)
+    @OneToOne(fetch = FetchType.LAZY, cascade = CascadeType.ALL, orphanRemoval = true)
     @JoinColumn(name = "teacher_more_info_id")
     private TeacherMoreInfo teacherMoreInfo;
 
@@ -45,7 +45,7 @@ public class Teacher {
         return Collections.unmodifiableSet(courses);
     }
 
-    public void addRegion() {
+    public void addRegion(Region region) {
         this.setRegion(region);
         region.addTeacher(this);
     }
