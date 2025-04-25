4f348e255f1bddf0ee898f0d6ecba649393c3bb4
Th. Androutsos <a8anassis@gmail.com>
2025-04-25 17:57:21 +0300

    feat: sql updates

---
+++ src/main/resources/sql/courses.sql
@@ -0,0 +1,14 @@
+use hiber6dev2db;
+
+INSERT INTO `courses` VALUES (1,'Java'),
+	(2,'C#'),
+	(3,'Databases'),
+	(4,'JavaScript'),
+	(5,'UX/UI'),
+	(6,'Git'),
+	(7,'Python'),
+	(8,'Angular'),
+	(9,'React'),
+	(10,'Vue');
+
+ALTER TABLE teachers AUTO_INCREMENT = 11;
---
+++ src/main/resources/sql/teacher_more_info.sql
@@ -0,0 +1,7 @@
+use hiber6dev2db;
+
+INSERT INTO `teacher_more_info` VALUES (1,'1991-01-12 09:30:00','MALE'),
+	(2,'2000-11-10 21:10:00','FEMALE');
+    
+    ALTER TABLE `teacher_more_info` AUTO_INCREMENT = 3;
+    
