98568c122714a0df7e12cb24a1a1c3474d8f8d4f
Th. Androutsos <a8anassis@gmail.com>
2025-04-25 19:30:21 +0300

    fix: insert into courses. Comments added

--- src/main/resources/sql/courses.sql
+++ src/main/resources/sql/courses.sql
@@ -1,14 +1,15 @@
-use hiber6dev2db;
+USE hiber7pro2db;
 
-INSERT INTO `courses` VALUES (1,'Java'),
-	(2,'C#'),
-	(3,'Databases'),
-	(4,'JavaScript'),
-	(5,'UX/UI'),
-	(6,'Git'),
-	(7,'Python'),
-	(8,'Angular'),
-	(9,'React'),
-	(10,'Vue');
+INSERT INTO `courses` (id, title, comments, lesson_type) VALUES (1,'Java', '', 1),
+	(2,'C#', '', 2),
+	(3,'Python', '', 3),
+	(4,'JavaScript', '', 1),
+	(5,'TypeScript', '', 2),
+	(6,'Scala', '', 3),
+	(7,'Go', '', 1),
+	(8,'SQL', '', 3),
+	(9,'React', '', 2),
+	(10,'Angular', '', 3);
 
-ALTER TABLE teachers AUTO_INCREMENT = 11;
+ALTER TABLE courses AUTO_INCREMENT = 11;
+
